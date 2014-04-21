939741694936c0f382235695f5dc2ce2b3855ec8
Zachary Watson <zwatson@EE-ZWatson.local>
2014-04-21 12:18:56 -0700

    added install script, makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+install:
+	chmod a+x install.sh
+	./install.sh
+
+uninstall:
+	chmod a+x uninstall.sh
+	./uninstall.sh
--- uninstall.sh
+++ uninstall.sh
@@ -12,11 +12,6 @@ files="vimrc"                     # list of files/folders to symlink in homedir
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
@@ -24,8 +19,15 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file $olddir
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+  echo "Removing symlink to $file in home directory."
+  rm ~/.$file
+  echo "Moving any existing backup dotfiles from $olddir to ~"
+  mv $olddir/.$file ~
 done
+
+# create dotfiles_old in homedir
+echo "Removing $olddir"
+rmdir $olddir
+echo "...done"
+
+
